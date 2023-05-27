e78890e5ef05e627fd0eb22b6a8f72b101490d27
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-27 03:00:40 +0300

    Added comments

--- article.rb
+++ article.rb
@@ -33,11 +33,11 @@ class Author
   def self.all
     @@all
   end
-
+#Returns an array of Article instances the author has written
   def articles
     Article.all.select { |article| article.author == self }
   end
-
+#Returns a unique array of Magazine instances for which the author has contributed to
   def magazines
     articles.map { |article| article.magazine }
   end
@@ -56,7 +56,7 @@ class Magazine
   def self.all
     @@all
   end
-#returns an array of magazine contributors
+#Returns an array of Author instances who have written for this magazine
   def contributors
     Article.all.select { |article| article.magazine == self }.map { |article| article.author }
   end
